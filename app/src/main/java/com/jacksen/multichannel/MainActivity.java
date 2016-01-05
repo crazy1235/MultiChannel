2302b5b70908d063ead53ce109727551de2a2cc0
jacksen <951532416@qq.com>
2016-01-06 00:27:42 +0800

    update

--- app/src/main/java/com/jacksen/multichannel/MainActivity.java
+++ app/src/main/java/com/jacksen/multichannel/MainActivity.java
@@ -1,5 +1,6 @@
 package com.jacksen.multichannel;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +28,13 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        init();
+    }
+
+    private void init() {
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("BuildConfig.APP_TYPE : " + BuildConfig.APP_TYPE);
     }
 
     @Override
